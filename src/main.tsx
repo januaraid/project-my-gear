f758f076fa17534d75add84340663651f33385fa
januaraid <y.ryouske@gmail.com>
2023-12-30 22:05:16 +0900

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app); // eslint-disable-line
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
